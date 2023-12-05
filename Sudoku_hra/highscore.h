e3f973ba617486d305728cb9f968f719617c7eb0
hanastepanova <145663784+hanastepanova@users.noreply.github.com>
2023-12-05 19:23:35 +0100

    Update highscore.h
    Final version

--- Sudoku_hra/highscore.h
+++ Sudoku_hra/highscore.h
@@ -1,19 +1,45 @@
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
 
-// Adjust the structure and function declarations based on your requirements
+#pragma once;
+#include <stdio.h>
 
 typedef struct {
-    char jmeno[50];
-    double score;
+	char jmeno[50];
+	double score;
 } scoreEntry;
 
 typedef struct {
-    scoreEntry entries[5];  // Assuming you want to store the top 5 highscores
-    int count;  // Number of entries in the highscore board
+	scoreEntry entries[5];
+	int count;
 } score;
 
-void display_highscores(const score *score);
-void update_highscores(score *score, const char *jmeno, int score);
+//void displayHighscore(const score* score);
+void updateHighscore(double score) {
 
-#endif // HIGHSCORE_H
+
+
+	char jmeno[50];
+	printf("Zadejte Vase jmeno: ");
+	scanf("%s", jmeno);
+
+	printf("Highscoreboard si muzete prohlednout v souboru: vystuniSoubor\n"); 
+
+
+	const char* vystuniCesta = "C:\\Users\\42060\\source\\repos\\Sudoku_hra\\vystuniSoubor.txt";
+
+
+	FILE* file;
+
+	if (fopen_s(&file, vystuniCesta, "a")) {
+		printf("Chyba v ulozeni score.\n");
+	}
+
+	fprintf(file, "%s, %lf\n", jmeno, score);
+	fclose(file);
+	printf("Score ulozeno bez problemu.\n");
+	
+	
+}
+
+#endif
