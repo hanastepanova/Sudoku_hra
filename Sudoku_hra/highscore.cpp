0bac82c206b865454e7debcc06fcf253b1ade2b9
hanastepanova <145663784+hanastepanova@users.noreply.github.com>
2023-12-05 19:27:55 +0100

    Update highscore.cpp
    Final version

--- Sudoku_hra/highscore.cpp
+++ Sudoku_hra/highscore.cpp
@@ -1,23 +1,4 @@
-
 #include "highscore.h"
 #include <stdio.h>
 
 // ... your existing functions ...
-
-void update_highscores(score *score, int score) {
-    printf("Your time: %d seconds.\n", score);
-
-    char jmeno[50];
-    printf("Enter your name: ");
-    scanf("%s", jmeno);
-
-    FILE *file = fopen("vystupniSoubor.txt", "a"); // Open file in append mode
-
-    if (file != NULL) {
-        fprintf(file, "%s %d\n", jmeno, score);
-        fclose(file);
-        printf("Highscore saved successfully!\n");
-    } else {
-        printf("Error saving highscore.\n");
-    }
-}
